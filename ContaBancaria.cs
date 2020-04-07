bdb5e485268c105af8c3e4bc598569b5d49c55d1
Renato Azevedo <renatoads1@gmail.com>
2020-04-07 20:22:52 -0300

    primeiro commit

---
+++ ContaBancaria.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class ContaBancaria
+    {
+        //prop +tab tab | privada por não poder ser modificada
+        public int numeroConta { get; private set; }
+        public string titularConta { get; private set; }
+        public float depositoInicial { get; private set; }
+        public float saldoConta { get; private set; }
+
+        public ContaBancaria(int numeroConta, string titularConta) 
+        {
+            this.numeroConta = numeroConta;
+            this.titularConta = titularConta;
+        }
+        public ContaBancaria(int numeroConta, string titularConta, float depositoInicial, float saldoConta) : this( numeroConta, titularConta)
+        {
+            this.depositoInicial = depositoInicial;
+            this.saldoConta = saldoConta;
+        }
+        public override string ToString()
+        {
+            return "Conta: "+numeroConta
+                +"Titular:"+titularConta
+                +"Saldo: "+saldoConta.ToString("F2",CultureInfo.InvariantCulture); 
+                //base.ToString();
+        }
+
+        public float Depositar(float valorDeposito) {
+            this.saldoConta = (this.saldoConta + valorDeposito);
+            return this.saldoConta;
+        }
+
+        public float Sacar(float valorDeposito)
+        {
+            this.saldoConta = (this.saldoConta - valorDeposito);
+            return this.saldoConta;
+        }
+
+
+
+
+        //clase construtora
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+            Console.Write("Entre com o nímero da conta");
+            int NumConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o Titular da conta");
+            string Titular = Console.ReadLine();
+            Console.Write("Havera deposito inicial? (S/N) ");
+            char resp = char.Parse(Console.ReadLine());
+            if ((resp == 'S') || (resp == 's'))
+            {
+                Console.Write("Digite o valor de deposito inicial");
+                float saldo = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(NumConta, Titular, saldo, saldo);
+            }
+            else
+            {
+                conta = new ContaBancaria(NumConta, Titular);
+            }
+
+            Console.WriteLine("conta aberta");
+            Console.WriteLine(conta);
+
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Para deposito digite 1 Para saque digite 2");
+            string escolha = Console.ReadLine();
+            if (escolha == "1")
+            {
+
+                Console.WriteLine("Digite o valor a ser depositado");
+                float valor = float.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                string resultado = conta.Depositar(valor).ToString();
+                Console.Clear();
+                Console.WriteLine(conta);
+            }
+            else
+            {
+                Console.WriteLine("Digite o valor a ser sacado");
+                float valor = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                string resultado = conta.Sacar(valor).ToString();
+                Console.Clear();
+                Console.WriteLine(conta);
+            }
+
+        }
+    }
+}
